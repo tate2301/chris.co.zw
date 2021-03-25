75444609bce3959e49ac2c2a9079b0e13e0c3aa5
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2021-03-25 17:36:13 +0200

    Publish

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main className="container mx-auto p-4">
         <div className="py-4">
-          <p className="text-3xl lg:text-6xl font-bold">
+          <p className="text-xl md:text-3xl lg:text-6xl font-bold">
             I’m Tatenda Christopher, a Software Engineer and 
             Designer based in Zimbabwe. Specializing in creating
             products for people, on the web and on mobile devices.
@@ -36,13 +36,13 @@ export default function Home() {
         </div>
 
         <div className="pt-8 lg:pt-12">
-          <p className="flex space-x-4">
-            <span className="bg-gray-100 rounded-full p-2">
-              <svg className="h-20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <p className="flex flex-wrap">
+            <span className="bg-gray-100 rounded-full p-2 mr-4">
+              <svg className="h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
               </svg>
             </span>
-            <span className="my-auto">
+            <span className="my-auto text-sm lg:text-md py-2 md:py-0">
               Currently available for freelance or full-time work
             </span>
           </p>
