ae0fdba74fb46facc7c38eeed22b6ead57207641
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2021-03-25 17:31:33 +0200

    Finished home

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,15 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <div className="text-gray-700">
       <Head>
-        <title>Create Next App</title>
+        <title>Tatenda Christopher | A Software Engineer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="container mx-auto p-4">
         <div className="py-4">
-          <p className="text-3xl lg:text-6xl font-bold text-gradient bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-500">
+          <p className="text-3xl lg:text-6xl font-bold">
             I’m Tatenda Christopher, a Software Engineer and 
             Designer based in Zimbabwe. Specializing in creating
             products for people, on the web and on mobile devices.
@@ -30,19 +30,19 @@ export default function Home() {
         </div>
 
         <div className="py-4">
-          <a className="font-medium text-gray-800 text-xl" href="#">
+          <a className="font-medium text-xl bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500" href="#">
             Have an exciting project? Let’s connect ->
           </a>
         </div>
 
         <div className="pt-8 lg:pt-12">
-          <p className="flex space-x-4 text-blue-500">
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <p className="flex space-x-4">
+            <span className="bg-gray-100 rounded-full p-2">
+              <svg className="h-20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
               </svg>
             </span>
-            <span>
+            <span className="my-auto">
               Currently available for freelance or full-time work
             </span>
           </p>
