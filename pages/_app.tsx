0847cf700526a944964bd505a1ea51498745e863
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2021-05-15 00:38:01 +0200

    Add Inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import 'tailwindcss/tailwind.css'
 import DefaultLayout from '../layouts/default'
 import '../styles/globals.css'
@@ -5,6 +6,9 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <DefaultLayout>
+      <Head>
+        <link rel={"preload"} href='https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap' />
+      </Head>
       <Component {...pageProps} />
     </DefaultLayout>
   )
