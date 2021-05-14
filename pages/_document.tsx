c0e6815a182edbcbb3a88520491c410b43fb1046
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2021-05-14 23:08:33 +0200

    ICP

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,68 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import React from 'react'
+import React from 'react';
+import { compact, flatten } from 'lodash';
+class NextScriptCustom extends NextScript {
+  render() {
+    const orgNextScripts = flatten(super.render().props.children);
 
-class DeferredNextScript extends NextScript {
-    getScripts(files) {
-      return super.getScripts(files).map(script => {
-        return React.cloneElement(script, {
-          key: script.props.src,
-          defer: true,
-          async: false,
-        })
+    const scripts = compact(
+      orgNextScripts.map((child) => {
+        if (child.props.id === '__NEXT_DATA__') {
+          return {
+            props: { ...child.props },
+            content: child.props.dangerouslySetInnerHTML.__html
+          };
+        }
+
+        if (child?.type === 'script') {
+          return {
+            props: { ...child.props },
+            content: ''
+          };
+        }
+
+        return null;
       })
-    }
-    getDynamicChunks(files) {
-      return super.getDynamicChunks(files).map(script => {
-        return React.cloneElement(script, {
-          key: script.props.src,
-          defer: true,
-          async: false,
-        })
-      })
-    }
+    );
+
+    const initialFilterer = props => !props.src || !props.src.includes('chunk');
+    const initialLoadScripts = scripts.filter(({ props }) => initialFilterer(props));
+    const chunkedScripts = scripts.filter(({ props }) => !initialFilterer(props));
+
+    const jsContent = `
+      var chunkedScripts = ${JSON.stringify(chunkedScripts)};
+      setTimeout(() => {
+        chunkedScripts.map((script) => {
+          if (!script || !script.props) return;
+          try {
+            var scriptTag = document.createElement('script');
+
+            scriptTag.src = script.props.src;
+            scriptTag.async = false;
+            scriptTag.defer = true;
+
+            if (script.props.id) scriptTag.id = script.props.id;
+            if (script.content) scriptTag.innerHTML = script.content;
+            document.body.appendChild(scriptTag);
+          }
+          catch(err) {
+            console.log(err);
+          }
+        });
+      // 1800ms seems like when PageSpeed Insights stop waiting for more js
+      }, 1800);
+    `;
+
+    return (
+      <>
+        {initialLoadScripts.map(({ props }) => (
+          !props?.src?.includes("framework") && <script key={props.id} {...props} src={props.src} />
+        ))}
+
+        <script id="__NEXT_SCRIPT_CUSTOM" defer dangerouslySetInnerHTML={{ __html: jsContent }} />
+      </>
+    );
+  }
 }
 
 
@@ -35,7 +78,7 @@ class MyDocument extends Document {
         <Head />
         <body>
           <Main />
-          <DeferredNextScript />
+          <NextScriptCustom />
         </body>
       </Html>
     )
