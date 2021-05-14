03cdb472920fc50edcf96249e875f370a9e74246
Tatenda Christopher Chinyamakobvu <chris@devtachom.com>
2021-05-14 22:51:43 +0200

    ICP

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main } from 'next/document'
+import { FeaturePolyfills, NextScript } from '@engineerapart/nextscript';
 
+const features = [
+    FeaturePolyfills.FETCH, // ensures ES6 fetch is available
+    FeaturePolyfills.CUSTOMEVENT, // ensures custom event is available
+    { // example of how to add any other type of polyfill
+      test: `('entries' in Array.prototype)`,
+      feature: 'Array.prototype.entries',
+    },
+  ];
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -12,7 +21,7 @@ class MyDocument extends Document {
         <Head />
         <body>
           <Main />
-          <NextScript />
+          <NextScript features={features} />
         </body>
       </Html>
     )
